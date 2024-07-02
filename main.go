5f55cb5b152b2cb5984f49849c98392c167bfc25
Jake Jasmin <roastbeefer000@gmail.com>
2024-07-02 06:38:13 -0600

    use bubbletea with viewport

--- main.go
+++ main.go
@@ -5,10 +5,13 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 
-	"github.com/charmbracelet/lipgloss"
 	"github.com/RoastBeefer00/recipes-cli/list"
-    fuzzyfinder "github.com/ktr0731/go-fuzzyfinder"
+	tea "github.com/charmbracelet/bubbletea"
+    "github.com/charmbracelet/bubbles/viewport"
+	"github.com/charmbracelet/lipgloss"
+	fuzzyfinder "github.com/ktr0731/go-fuzzyfinder"
 )
 
 type Recipe struct {
@@ -16,6 +19,12 @@ type Recipe struct {
 	Time        string   `json:"Time"`
 	Ingredients []string `json:"Ingredients"`
 	Steps       []string `json:"Steps"`
+}
+
+type model struct {
+    ready bool
+	view string // items on the to-do list
+    viewport viewport.Model
 }
 
 const rosewater string = "#f5e0dc"
@@ -49,35 +58,94 @@ var titleStyle = lipgloss.NewStyle().
 	Bold(true).
 	Foreground(lipgloss.Color(crust)).
 	Background(lipgloss.Color(sky)).
-    PaddingLeft(2).
-    PaddingRight(2).
-    PaddingTop(1).
-    PaddingBottom(1).
-    MarginLeft(1).
-    Align(lipgloss.Center)
-    
+	PaddingLeft(2).
+	PaddingRight(2).
+	PaddingTop(1).
+	PaddingBottom(1).
+	MarginLeft(1).
+	Align(lipgloss.Center)
+
 var timeStyle = lipgloss.NewStyle().
-    Foreground(lipgloss.Color(text)).
-    Align(lipgloss.Center).MarginLeft(1)
+	Foreground(lipgloss.Color(text)).
+	Align(lipgloss.Center).MarginLeft(1)
 
 var listStyle = lipgloss.NewStyle().
-    Bold(true).
-    MarginTop(2).
-    PaddingLeft(1).
-    PaddingRight(1).
-    MarginLeft(1).
-    MarginBottom(1).
-    Foreground(lipgloss.Color(crust)).
-    Background(lipgloss.Color(green)).
-    Align(lipgloss.Center)
+	Bold(true).
+	MarginTop(2).
+	PaddingLeft(1).
+	PaddingRight(1).
+	MarginLeft(1).
+	MarginBottom(1).
+	Foreground(lipgloss.Color(crust)).
+	Background(lipgloss.Color(green)).
+	Align(lipgloss.Center)
 
 var itemStyle = lipgloss.NewStyle().
-    Width(80).PaddingTop(1).MarginLeft(2)
+	Width(80).PaddingTop(1).MarginLeft(2)
 
 var numStyle = lipgloss.NewStyle().
-    PaddingTop(1).MarginLeft(2)
+	PaddingTop(1).MarginLeft(2)
 
 var recipes []Recipe
+
+func (m model) Init() tea.Cmd {
+	// Just return `nil`, which means "no I/O right now, please."
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var (
+		cmd  tea.Cmd
+		cmds []tea.Cmd
+	)
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		if k := msg.String(); k == "ctrl+c" || k == "q" || k == "esc" {
+			return m, tea.Quit
+		}
+
+	case tea.WindowSizeMsg:
+
+		if !m.ready {
+			// Since this program is using the full size of the viewport we
+			// need to wait until we've received the window dimensions before
+			// we can initialize the viewport. The initial dimensions come in
+			// quickly, though asynchronously, which is why we wait for them
+			// here.
+			m.viewport = viewport.New(msg.Width, msg.Height)
+			m.viewport.HighPerformanceRendering = false
+			m.viewport.SetContent(m.view)
+			m.ready = true
+
+		} else {
+			m.viewport.Width = msg.Width
+			m.viewport.Height = msg.Height
+		}
+
+		// if useHighPerformanceRenderer {
+			// Render (or re-render) the whole viewport. Necessary both to
+			// initialize the viewport and when the window is resized.
+			//
+			// This is needed for high-performance rendering only.
+			// cmds = append(cmds, viewport.Sync(m.viewport))
+		// }
+	}
+
+	// Handle keyboard and mouse events in the viewport
+	m.viewport, cmd = m.viewport.Update(msg)
+	cmds = append(cmds, cmd)
+
+	return m, tea.Batch(cmds...)
+}
+
+func (m model) View() string {
+	if !m.ready {
+		return "Loading..."
+	}
+
+	return m.viewport.View()
+}
 
 func main() {
 	client := http.Client{}
@@ -98,41 +166,56 @@ func main() {
 		panic(err)
 	}
 
-    idx, err := fuzzyfinder.FindMulti(
-        recipes,
-        func(i int) string {
-            return fmt.Sprintf("%s (%s)", recipes[i].Name, recipes[i].Time)
-        },
-        fuzzyfinder.WithPreviewWindow(func(i, w, h int) string {
-            if i == -1 {
-                return ""
-            }
-            ing := list.New(recipes[i].Ingredients).Enumerator(list.Bullet)
-            s := list.New(recipes[i].Steps).Enumerator(list.Arabic).ItemStyle(itemStyle).EnumeratorStyle(numStyle)
-            return fmt.Sprintf("%s \n%s\n\n%s\n%s\n\n%s\n%s",
-                titleStyle.Render(recipes[i].Name),
-                timeStyle.Render(recipes[i].Time),
-                listStyle.Render("Ingredients"),
-                ing.EnumeratorStyle(lipgloss.NewStyle().MarginLeft(1)).ItemStyle(lipgloss.NewStyle().MarginLeft(1)),
-                listStyle.Render("Steps"),
-                s,
-                )
-        }))
-    if err != nil {
-        panic(err)
-    }
+	idx, err := fuzzyfinder.FindMulti(
+		recipes,
+		func(i int) string {
+			return fmt.Sprintf("%s (%s)", recipes[i].Name, recipes[i].Time)
+		},
+		fuzzyfinder.WithPreviewWindow(func(i, w, h int) string {
+			if i == -1 {
+				return ""
+			}
+			ing := list.New(recipes[i].Ingredients).Enumerator(list.Bullet)
+			s := list.New(recipes[i].Steps).Enumerator(list.Arabic).ItemStyle(itemStyle).EnumeratorStyle(numStyle)
+			return fmt.Sprintf("%s \n%s\n\n%s\n%s\n\n%s\n%s",
+				titleStyle.Render(recipes[i].Name),
+				timeStyle.Render(recipes[i].Time),
+				listStyle.Render("Ingredients"),
+				ing.EnumeratorStyle(lipgloss.NewStyle().MarginLeft(1)).ItemStyle(lipgloss.NewStyle().MarginLeft(1)),
+				listStyle.Render("Steps"),
+				s,
+			)
+		}))
+	if err != nil {
+		panic(err)
+	}
 
-    for _, i := range idx {
-        recipe := recipes[i]
-        ing := list.New(recipe.Ingredients).Enumerator(list.Bullet)
-        s := list.New(recipe.Steps).Enumerator(list.Arabic).ItemStyle(itemStyle).EnumeratorStyle(numStyle)
+	var view string
 
-        fmt.Println(titleStyle.Render(recipe.Name))
-        fmt.Println(timeStyle.Render(recipe.Time))
-        fmt.Println(listStyle.Render("Ingredients"))
-        fmt.Println(ing.EnumeratorStyle(lipgloss.NewStyle().MarginLeft(1)).ItemStyle(lipgloss.NewStyle().MarginLeft(1)))
-        fmt.Println(listStyle.Render("Steps"))
-        fmt.Println(s)
+	for _, i := range idx {
+		recipe := recipes[i]
+		ing := list.New(recipe.Ingredients).Enumerator(list.Bullet)
+		s := list.New(recipe.Steps).Enumerator(list.Arabic).ItemStyle(itemStyle).EnumeratorStyle(numStyle)
 
-    }
+		// fmt.Println(titleStyle.Render(recipe.Name))
+		view = view + fmt.Sprintf("%s\n%s\n\n%s\n%s\n\n%s\n%s", 
+            titleStyle.Render(recipe.Name), 
+            timeStyle.Render(recipe.Time), 
+            listStyle.Render("Ingredients"), 
+            ing.EnumeratorStyle(lipgloss.NewStyle().MarginLeft(1)).ItemStyle(lipgloss.NewStyle().MarginLeft(1)), 
+            listStyle.Render("Steps"), 
+            s)
+		// fmt.Println(timeStyle.Render(recipe.Time))
+		// fmt.Println(listStyle.Render("Ingredients"))
+		// fmt.Println(ing.EnumeratorStyle(lipgloss.NewStyle().MarginLeft(1)).ItemStyle(lipgloss.NewStyle().MarginLeft(1)))
+		// fmt.Println(listStyle.Render("Steps"))
+		// fmt.Println(s)
+
+        p := tea.NewProgram(model{view: view}, tea.WithAltScreen())
+        if _, err := p.Run(); err != nil {
+            fmt.Printf("Alas, there's been an error: %v", err)
+            os.Exit(1)
+        }
+
+	}
 }
